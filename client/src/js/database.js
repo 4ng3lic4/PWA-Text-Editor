58a1a3458206cf53fdaf06c18078c1e71b650654
4ng3lic4 <ositosviajeros@hotmail.com>
2023-02-02 12:35:57 -0500

    debugging server.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,35 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+//We are creating a new database named 'jate' which will be using version 1 of the database.
   openDB('jate', 1, {
+    //Add our database schema if it has not already been initialized.
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+
+      //Create a new object store for the data and give it a key name of 'id' which needs to increment automatically.
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
+
+
+
+  //Review Mini project database.js  to see if I need to add to this code
+
+
 // TODO: Add logic to a method that accepts some content and adds it to the database
+//
 export const putDb = async (content) => console.error('putDb not implemented');
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 
 initdb();
+ 
