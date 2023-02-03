284a626053da83ea7ddbe6ebed30b9fc73a2e57a
4ng3lic4 <ositosviajeros@hotmail.com>
2023-02-03 18:31:01 -0500

    updating install.js with post and get

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -25,7 +25,7 @@ butInstall.addEventListener('click', async () => {
 
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// : Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
 
     window.deferredPrompt = null;
